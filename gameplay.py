a33d8fb4234699705b1266416029c7b4f3377e86
sidhu1012 <45732670+sidhu1012@users.noreply.github.com>
2020-03-16 15:53:46 +0530

    Add files via upload

--- gameplay.py
+++ gameplay.py
@@ -12,8 +12,8 @@ import random as rn
 # In[4]:
 
 
-def question_number(l):
-    j=rn.randint(50)
+def question_number(l,n):
+    j=rn.randint(n)
     if j in l:
         question(l)
     else:
@@ -48,12 +48,12 @@ def show(q,tries):
 # In[6]:
 
 
-def game_play(tries):
+def game_play(tries,n):
     hints=3
     l=[]
     for i in range(10):
         print(f'Quest.{i}')
-        s=question_number(l)
+        s=question_number(l,n)
         quest =list(movie_name[s])
         show(quest,tries)
         
@@ -68,7 +68,8 @@ def hollywood_easy():
     movie_name=np.asanyarray(df[['Name']])
     star_cast=np.asanyarray(df[['Star Cast']])
     release=np.asanyarray(df[['Year']])
-    game_play(3)
+    n=len(movie_name)
+    game_play(3,n)
     
 
         
@@ -77,7 +78,29 @@ def hollywood_easy():
 # In[ ]:
 
 
+def hollywood_medium():
+    df=pd.read_csv('C:\\Users\sudee\OneDrive\Desktop\Hollywood-medium.csv')
+    df.head()
+    movie_name=np.asanyarray(df[['Name']])
+    star_cast=np.asanyarray(df[['Star Cast']])
+    release=np.asanyarray(df[['Year']])
+    n=len(movie_name)
+    game_play(4,n)
+    
 
+
+# In[1]:
+
+
+def hollywood_hard():
+    df=pd.read_csv('C:\\Users\sudee\OneDrive\Desktop\Hollywood-hard.csv')
+    df.head()
+    movie_name=np.asanyarray(df[['Name']])
+    star_cast=np.asanyarray(df[['Star Cast']])
+    release=np.asanyarray(df[['Year']])
+    n=len(movie_name)
+    game_play(5,n)
+    
 
 
 # In[ ]:
