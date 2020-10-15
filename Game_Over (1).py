1cb03d0a06fc70e24917ed7a5c3b5f199602b466
vibhorchatwani <59525358+vibhorchatwani@users.noreply.github.com>
2020-10-15 10:41:00 +0530

    Add files via upload

---
+++ Game_Over (1).py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def game_over(total_score):  
+      print( "Game Over! \n\n\n")
+      print("     THANKS FOR PLAYING WITH US !! \n\n\n    ")
+      print("      YOUT TOTAL SCORE IS",total_score)
+
